fb6871836864e9f0a55b7186495844a0d4667754
Ergpopler <theodorekaczynski@ctemplar.com>
2021-08-22 01:37:41 -0400

    Format

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@ use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow};
 
 fn main() {
-    let app = Application::builder()
-        .application_id("org.gtk.example")
-        .build();
+	let app = Application::builder()
+		.application_id("org.gtk.example")
+		.build();
 
-    app.connect_activate(build_ui);
+	app.connect_activate(build_ui);
 
-    // Run the application
-    app.run();
+	// Run the application
+	app.run();
 }
 
 fn build_ui(app: &Application) {
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Murasaki")
-        .build();
+	let window = ApplicationWindow::builder()
+		.application(app)
+		.title("Murasaki")
+		.build();
 
-    window.present();
+	window.present();
 }
