18f652e47290e82d1d395ef4555a43c5a06735aa
Ergpopler <theodorekaczynski@ctemplar.com>
2021-08-23 19:52:25 -0400

    Fullscreen support

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@
 */
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow};
-use gtk::{Box, Button};
-use gtk::prelude::BoxExt;
+use gtk::{Box, Button, Inhibit};
+use gtk::prelude::{WidgetExt, ButtonExt, BoxExt};
 
 use webkit2gtk::{WebContext, WebView};
-use webkit2gtk::traits::WebViewExt;
+use webkit2gtk::traits::{WebViewExt, WindowPropertiesExt};
 
 fn main() {
 	gtk::init().unwrap();
@@ -42,7 +42,7 @@ fn build_ui(app: &Application) {
 	let back_button = Button::builder()
 		.label("Back")
 		.margin_top(0)
-		.margin_bottom(0)
+		.margin_bottom(1000)
 		.margin_start(0)
 		.margin_end(0)
 		.build();
@@ -50,7 +50,7 @@ fn build_ui(app: &Application) {
 	let next_button = Button::builder()
 		.label("Next")
 		.margin_top(0)
-		.margin_bottom(0)
+		.margin_bottom(1000)
 		.margin_start(0)
 		.margin_end(0)
 		.build();
@@ -63,10 +63,44 @@ fn build_ui(app: &Application) {
 	back_button.connect_clicked(move |_y| {
 		webview_back.go_back();
 	});
-	vbox.pack_start(&back_button, false, false, 5);
-	vbox.pack_start(&next_button, false, false, 5);
+	vbox.pack_start(&back_button, false, true, 3);
+	vbox.pack_start(&next_button, false, true, 3);
 	vbox.pack_start(&webview, true, true, 0);
 	window.show_all();
 
 	window.present();
+	let screen = window.screen();
+	let screen = match screen {
+		Some(s) => s,
+		None => panic!("No Screen Found"),
+	};
+	let screen_size = (screen.width(), screen.height());
+	window.connect_configure_event(move |x, _y| {
+		if x.size() == screen_size {
+			back_button.set_visible(false);
+			next_button.set_visible(false);
+		} else {
+			back_button.set_visible(true);
+			next_button.set_visible(true);
+		}
+		false
+	});
+
+	// The following is just for testing purposes, really.
+	/*
+	window.connect_key_press_event(move |_x, y| {
+		if let state = y.state() {
+			println!("Control key pressed 😳: {}", state);
+		}
+		let keyvalue = y.keyval().to_unicode();
+		match keyvalue {
+			Some(v) => {
+				println!("Key Pressed 😳: {}", v)
+			}
+			None => {}
+		};
+
+		Inhibit(false)
+	});
+	*/
 }
